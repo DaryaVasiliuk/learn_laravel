386190f033044013429d14a4bbb7f955ab7d3d5f
Веня Vasilyuk <dashka.chernichka14@gmail.com>
2023-06-06 12:37:53 +0300

    8

---
+++ app/Http/Controllers/MyPlaceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MyPlaceController extends Controller
+{
+    public function index() {
+        return 'this is my page';
+    }
+
+    public function bla() {
+
+    }
+}
